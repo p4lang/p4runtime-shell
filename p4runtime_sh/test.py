74ace143731ac2749ec2e88ad501e56337bc2c03
Kentaro Ebisawa <ebiken.g@gmail.com>
2022-06-24 09:23:37 +0900

    Update test to expect iterator for MulticastGroupEntry and CloneSessionEntry

--- p4runtime_sh/test.py
+++ p4runtime_sh/test.py
@@ -1047,8 +1047,7 @@ multicast_group_entry {
         self.servicer.Write.assert_called_once_with(ProtoCmp(expected_req), ANY)
 
         self.simple_read_check(
-            expected_req.updates[0].entity, mcge, P4RuntimeEntity.packet_replication_engine_entry,
-            expect_iterator=False)
+            expected_req.updates[0].entity, mcge, P4RuntimeEntity.packet_replication_engine_entry)
 
     def test_multicast_group_entry_invalid(self):
         mcge = sh.MulticastGroupEntry()
@@ -1087,8 +1086,7 @@ clone_session_entry {
         self.servicer.Write.assert_called_once_with(ProtoCmp(expected_req), ANY)
 
         self.simple_read_check(
-            expected_req.updates[0].entity, cse, P4RuntimeEntity.packet_replication_engine_entry,
-            expect_iterator=False)
+            expected_req.updates[0].entity, cse, P4RuntimeEntity.packet_replication_engine_entry)
 
     def test_p4runtime_api_version(self):
         version = sh.APIVersion()
